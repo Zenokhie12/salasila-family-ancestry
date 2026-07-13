ed05064a9ac39cddaeb56fec27e2908abd335764
Zenokhie12 <Zenokhieasgar1@gmail.com>
2026-07-13 21:50:52 +0800

    Add typhography for convient use of fonsStyles

---
+++ theme/typography.ts
@@ -0,0 +1,21 @@
+export const typography = {
+    heading: {
+        fontSize: 22,
+        fontWieght: '700' as const,
+    },
+
+    subheading: {
+        fontSize: 16, 
+        fontWieght: '600' as const,
+    },
+
+    body: {
+        fontSize: 14,
+        fontWieght: '400' as const,
+    },
+    
+    caption: {
+        fontSize: 12,
+        fontWieght: '400' as const,
+    }
+};
