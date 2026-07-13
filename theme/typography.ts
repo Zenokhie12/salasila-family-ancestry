fbd5c2363bdd3f1fb28297bcdab166f1f9b91473
Zenokhie12 <Zenokhieasgar1@gmail.com>
2026-07-13 22:02:34 +0800

    add font styles

--- theme/typography.ts
+++ theme/typography.ts
@@ -2,20 +2,24 @@ export const typography = {
     heading: {
         fontSize: 22,
         fontWieght: '700' as const,
+        fontFamily: 'cinzel decorative' as const,
     },
 
     subheading: {
         fontSize: 16, 
         fontWieght: '600' as const,
+        fontFamily: 'cinzel' as const,
     },
 
     body: {
         fontSize: 14,
         fontWieght: '400' as const,
+        fontFamily: 'inter sans-serif' as const,
     },
     
     caption: {
         fontSize: 12,
         fontWieght: '400' as const,
+        fontFamily: 'italic' as const,
     }
 };
