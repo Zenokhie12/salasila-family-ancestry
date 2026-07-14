d288f46af39b6edbb6798001dfbd0d606d9f7fe6
Zenokhie12 <Zenokhieasgar1@gmail.com>
2026-07-14 18:24:39 +0800

    add create, edit, & delete

--- storage/people.ts
+++ storage/people.ts
@@ -32,3 +32,30 @@ export async function addPerson(input: NewPersonInput): Promise<Person> {
 
   return newPerson;
 }
+
+export async function updatePerson(id: string, updatedFields: Partial<Person>): Promise<Person> {
+  const people = await getAllPeople();
+
+  const existingPerson = people.find(p => p.id === id);
+  if (!existingPerson) {
+    throw new Error(`Person with id ${id} not found`);
+  }
+
+  const mergedPerson = { ...existingPerson, ...updatedFields };
+  const updatedPeople = people.map(p => (p.id === id ? mergedPerson : p));
+  await AsyncStorage.setItem(STORAGE_KEY, JSON.stringify(updatedPeople));
+
+  return mergedPerson;
+};
+
+export async function deletePerson(id: string): Promise<void> {
+  const people = await getAllPeople();
+
+  const findPerson = people.find(p => p.id === id);
+  if (!findPerson) {
+    throw new Error(`Person with id ${id} not found`);
+  }
+
+  const updatedPeople = people.filter(p => p.id !== id);
+  await AsyncStorage.setItem(STORAGE_KEY, JSON.stringify(updatedPeople));
+};
