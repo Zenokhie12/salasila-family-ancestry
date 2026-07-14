36516de8f4ebbbaf49a74b1fe533e74192e2fe8d
Zenokhie12 <Zenokhieasgar1@gmail.com>
2026-07-14 16:46:17 +0800

    add newPerson to accept parameters from addPerson

--- storage/people.ts
+++ storage/people.ts
@@ -1,5 +1,6 @@
 import AsyncStorage from '@react-native-async-storage/async-storage';
-import { Person } from '../types';
+import { Person, NewPersonInput } from '../types';
+import Crypto from 'expo-crypto';
 
 const STORAGE_KEY = 'salasila_people';
 
@@ -7,3 +8,27 @@ export async function getAllPeople(): Promise<Person[]> {
     const data = await AsyncStorage.getItem(STORAGE_KEY);
     return data ? JSON.parse(data) : [];
 };
+
+export async function addPerson(input: NewPersonInput): Promise<Person> {
+  const people = await getAllPeople();
+
+  const newPerson: Person = {
+    id: Crypto.randomUUID(),
+    fullName: input.fullName,
+    nickName: input.nickName,
+    honorificTitle: input.honorificTitle,
+    clanName: input.clanName,
+    occupation: input.occupation,
+    isDeceased: input.isDeceased,
+    dateOfBirth: input.dateOfBirth,
+    placeOfBirth: input.placeOfBirth,
+    currentAddress: input.currentAddress,
+    spouseIds: [],
+    media: [],
+  };
+
+  const updatedPeople = [...people, newPerson];
+  await AsyncStorage.setItem(STORAGE_KEY, JSON.stringify(updatedPeople));
+
+  return newPerson;
+}
--- types/person.ts
+++ types/person.ts
@@ -4,6 +4,13 @@ export type Person = {
     id: string;
     fullName: string;
 
+    // Optional inputs
+    nickName?: string;
+    honorificTitle?: string; // e.g., Mr., Mrs., Dr., Hadji, Datu
+    clanName?: string;       // Ancestral House / Branch Family Name
+    occupation?: string;
+    isDeceased?: boolean;
+
     dateOfBirth?: string;
     placeOfBirth?: string;
     currentAddress?: string;
@@ -19,24 +26,14 @@ export type Person = {
 
 export type NewPersonInput = {
     fullName: string;
-    nickName?: string;
-    honorificTitle?: string; // e.g., Mr., Mrs., Dr.,Hadji, Datu etc.
-    clanName?: string; // e.g, Ancestral House or Branch Family Name
 
+    nickName?: string;
+    honorificTitle?: string;
+    clanName?: string;
+    occupation?: string;
+    isDeceased?: boolean;
 
     dateOfBirth?: string;
     placeOfBirth?: string;
     currentAddress?: string;
-
-    isDeceased?: boolean;
-
-    occupation?: string; // Life long occupation or profession
-
-    lineAge: 'paternal' | 'maternal' | 'both';
-    fatherId?: string;
-    motherId?: string;
-    spouseIds: string[];
-    childrenIds: string[];
-
-    media: MediaAttachment[];
 };
